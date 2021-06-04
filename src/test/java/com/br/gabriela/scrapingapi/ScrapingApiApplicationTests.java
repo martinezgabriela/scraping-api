d55922dce697a1777b52030a7f49fa0ef86d9725
Gabriela Martinez <gabrielamartinez.19@gmail.com>
2021-06-03 21:34:03 -0300

    first commit

---
+++ src/test/java/com/br/gabriela/scrapingapi/ScrapingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gabriela.scrapingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
